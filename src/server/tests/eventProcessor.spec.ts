3b2bbf6d10ecdb9f8d5b88b77af582aeccce0cf2
Rafael Del Sole <rafael.silva@guiabolso.com.br>
2020-03-08 13:46:24 -0300

    adding Index

---
+++ src/server/tests/eventProcessor.spec.ts
@@ -0,0 +1,204 @@
+import { EventProcessor } from "../eventProcessor";
+import { Event, createEvent } from "../../client/events";
+import { buildResponseEventFor, buildResponseEventErrorFor, GenericErrorType } from "../responseEventBuilder";
+
+
+//Setup event handler
+const simpleSuccessEventHandler = (event: Event) => {
+    return Promise.resolve(buildResponseEventFor(event, {"success" : 1}))    
+}
+
+const simpleErrorEventHandler = (event: Event) => {
+    return Promise.resolve(buildResponseEventErrorFor(event,GenericErrorType))    
+}
+
+EventProcessor.addHandler("event:test", 1, simpleSuccessEventHandler);
+EventProcessor.addHandler("event:test:error:for", 2, simpleErrorEventHandler);
+
+describe("Test event protocol handler", () => {
+
+    test("Should return success event handler", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+        
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{}}
+
+        return EventProcessor.processEvent(rawEvent).then((responseEvent: Event) => {
+            expect(responseEvent.name).toEqual(name+":response");
+            expect(responseEvent.version).toEqual(version);
+            expect(responseEvent.id).toEqual(id);
+            expect(responseEvent.flowId).toEqual(flowId);
+            expect(responseEvent.identity).toEqual(JSON.parse("{}"));
+            expect(responseEvent.auth).toEqual(JSON.parse("{}"));
+            expect(responseEvent.metadata).toEqual(JSON.parse("{}"));
+            expect(responseEvent.payload).toEqual(JSON.parse("{\"success\": 1}"));
+        });
+    });
+
+    test("Should return error event response when porcessed function decide to do it", () => {
+        
+        const name = "event:test:error:for";
+        const version = 2;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+        
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{}}
+
+        return EventProcessor.processEvent(rawEvent).then((responseEvent: Event) => {
+            expect(responseEvent.name).toEqual(name+":error");
+            expect(responseEvent.version).toEqual(version);
+            expect(responseEvent.id).toEqual(id);
+            expect(responseEvent.flowId).toEqual(flowId);
+            expect(responseEvent.identity).toEqual(JSON.parse("{}"));
+            expect(responseEvent.auth).toEqual(JSON.parse("{}"));
+            expect(responseEvent.metadata).toEqual(JSON.parse("{}"));
+            expect(responseEvent.payload).toEqual(JSON.parse("{}"));
+        });
+    });
+
+    test("Should return eventNotFound error on event handler when the event name has not been registered", () => {
+        
+        const name = "event:test:does:not:exist";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+        
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{}}
+
+        
+        
+        const expectedPayloadMessage = {
+            "code": "NO_EVENT_HANDLER_FOUND",
+            "parameters": {
+                "event": name,
+                "version": version
+            }
+        }
+        
+        return EventProcessor.processEvent(rawEvent).then((responseEvent: Event) => {
+            expect(responseEvent.name).toEqual("eventNotFound");
+            expect(responseEvent.version).toEqual(version);
+            expect(responseEvent.id).toEqual(id);
+            expect(responseEvent.flowId).toEqual(flowId);
+            expect(responseEvent.identity).toEqual(JSON.parse("{}"));
+            expect(responseEvent.auth).toEqual(JSON.parse("{}"));
+            expect(responseEvent.metadata).toEqual(JSON.parse("{}"));
+            expect(JSON.stringify(responseEvent.payload)).toEqual(JSON.stringify(expectedPayloadMessage));
+        });
+    });
+
+    test("Should return eventNotFound error on event handler when the event version has not been registered", () => {
+        
+        const name = "teste:evento";
+        const version = 2;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+        
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{}}
+        
+        const expectedPayloadMessage = {
+            "code": "NO_EVENT_HANDLER_FOUND",
+            "parameters": {
+                "event": name,
+                "version": version
+            }
+        }
+        
+        return EventProcessor.processEvent(rawEvent).then((responseEvent: Event) => {
+            expect(responseEvent.name).toEqual("eventNotFound");
+            expect(responseEvent.version).toEqual(version);
+            expect(responseEvent.id).toEqual(id);
+            expect(responseEvent.flowId).toEqual(flowId);
+            expect(responseEvent.identity).toEqual(JSON.parse("{}"));
+            expect(responseEvent.auth).toEqual(JSON.parse("{}"));
+            expect(responseEvent.metadata).toEqual(JSON.parse("{}"));
+            expect(JSON.stringify(responseEvent.payload)).toEqual(JSON.stringify(expectedPayloadMessage));
+        });
+    });
+
+    test("Should return eventNotFound error on event handler when the event name and version has not been registered", () => {
+        
+        const name = "teste:evento:not:exists";
+        const version = 2;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+        
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{}}
+        
+        const expectedPayloadMessage = {
+            "code": "NO_EVENT_HANDLER_FOUND",
+            "parameters": {
+                "event": name,
+                "version": version
+            }
+        }
+        
+        return EventProcessor.processEvent(rawEvent).then((responseEvent: Event) => {
+            expect(responseEvent.name).toEqual("eventNotFound");
+            expect(responseEvent.version).toEqual(version);
+            expect(responseEvent.id).toEqual(id);
+            expect(responseEvent.flowId).toEqual(flowId);
+            expect(responseEvent.identity).toEqual(JSON.parse("{}"));
+            expect(responseEvent.auth).toEqual(JSON.parse("{}"));
+            expect(responseEvent.metadata).toEqual(JSON.parse("{}"));
+            expect(JSON.stringify(responseEvent.payload)).toEqual(JSON.stringify(expectedPayloadMessage));
+        });
+    });
+    
+    test("Should return badProtocol and property missing on payload when id field is missing", () => {
+        
+        const name = "teste:evento";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+        const auth = {}
+        const metadata = {}
+        const payload = {}
+        
+        const completePayload = {name, version, id, flowId, identity, auth, metadata, payload}
+
+        Object.keys(completePayload).forEach(key => {
+            if (key == "name") {
+                return
+            }
+            const objectToTest = {...completePayload}
+            delete objectToTest[key]
+
+            let expectedJsonPayload = <JSON><unknown>{
+                "code": "INVALID_COMMUNICATION_PROTOCOL",
+                "parameters": {
+                    "missingProperty": `${key}`
+                }
+            }
+
+            const promise = EventProcessor.processEvent(objectToTest)
+
+            Promise.resolve(promise).then((responseEvent: Event) => {
+                expect(responseEvent.name).toEqual("badProtocol");
+                expect(responseEvent.version).toEqual(version);
+                if (key != "id") {
+                    expect(responseEvent.id).toEqual(id);
+                }
+                if (key != "flowId") {
+                    expect(responseEvent.flowId).toEqual(flowId);
+                }
+                expect(responseEvent.identity).toEqual(JSON.parse("{}"));
+                expect(responseEvent.auth).toEqual(JSON.parse("{}"));
+                expect(responseEvent.metadata).toEqual(JSON.parse("{}"));
+                expect(JSON.stringify(responseEvent.payload)).toEqual(JSON.stringify(expectedJsonPayload));
+            });
+        })
+    });
+
+
+
+});
---
+++ src/server/tests/responseEventBuilder.spec.ts
@@ -0,0 +1,169 @@
+import { buildResponseEventFor, buildResponseEventErrorFor, GenericErrorType, BadRequest, Unauthorized, NotFound, Forbidden, UserDenied, ResourceDenied, Expired, NoEventFound, EventMessage, buildNoEventHandlerFor, buildBadProtocolFor } from "../responseEventBuilder";
+import { stringLiteral } from "@babel/types";
+
+describe("Test responseEventBuilder", () => {
+
+    test("Should build response event with empty payload when no value is passed in payload", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{"test":"test"}}
+
+        const responseEvent = buildResponseEventFor(rawEvent)
+
+        expect(responseEvent.name).toEqual(name+":response");
+        expect(responseEvent.version).toEqual(version);
+        expect(responseEvent.id).toEqual(id);
+        expect(responseEvent.flowId).toEqual(flowId);
+        expect(responseEvent.payload).toEqual({});
+        expect(responseEvent.auth).toEqual({});
+        expect(responseEvent.metadata).toEqual({});
+        expect(responseEvent.identity).toEqual({});
+    });
+
+    test("Should build response event with payload value when a value is passed in payload argument", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{"test":"test"}}
+
+        const responseEvent = buildResponseEventFor(rawEvent, "success")
+
+        expect(responseEvent.name).toEqual(name+":response");
+        expect(responseEvent.version).toEqual(version);
+        expect(responseEvent.id).toEqual(id);
+        expect(responseEvent.flowId).toEqual(flowId);
+        expect(responseEvent.payload).toEqual("success");
+        expect(responseEvent.auth).toEqual({});
+        expect(responseEvent.metadata).toEqual({});
+        expect(responseEvent.identity).toEqual({});
+    });
+
+    test("Should build response event error for each error type with payload value empty when no message is passed", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{"test":"test"}}
+
+        const errors = [GenericErrorType, BadRequest, Unauthorized, NotFound, Forbidden, UserDenied, ResourceDenied, Expired, NoEventFound]
+        
+        errors.forEach(error => {
+            const responseEvent = buildResponseEventErrorFor(rawEvent, error)
+            
+            expect(responseEvent.name).toEqual(`${name}:${error.typeName}`);
+            expect(responseEvent.version).toEqual(version);
+            expect(responseEvent.id).toEqual(id);
+            expect(responseEvent.flowId).toEqual(flowId);
+            expect(responseEvent.payload).toEqual({});
+            expect(responseEvent.auth).toEqual({});
+            expect(responseEvent.metadata).toEqual({});
+            expect(responseEvent.identity).toEqual({});
+        })
+        
+    });
+
+    test("Should build response event error with payload filled by value passed in message argument", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{"test":"test"}}
+
+        const params = new Map<string, any>()
+        params.set("value", 10)
+
+        const message: EventMessage =  {
+            "code": "TEST",
+            "parameters": params 
+          }
+
+        const responseEvent = buildResponseEventErrorFor(rawEvent, GenericErrorType, message)
+        
+        expect(responseEvent.name).toEqual(`${name}:${GenericErrorType.typeName}`);
+        expect(responseEvent.version).toEqual(version);
+        expect(responseEvent.id).toEqual(id);
+        expect(responseEvent.flowId).toEqual(flowId);
+        expect(responseEvent.payload).toEqual(message);
+        expect(responseEvent.auth).toEqual({});
+        expect(responseEvent.metadata).toEqual({});
+        expect(responseEvent.identity).toEqual({});
+    });
+
+    test("Should build response event error foventNotFound", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{"test":"test"}}
+
+        const message: EventMessage =  {
+            "code": "NO_EVENT_HANDLER_FOUND",
+            "parameters": {
+                "event": name,
+                "version" : version
+            } 
+          }
+
+        const responseEvent = buildNoEventHandlerFor(rawEvent)
+        
+        console.log(JSON.stringify(responseEvent))
+
+        expect(responseEvent.name).toEqual("eventNotFound");
+        expect(responseEvent.version).toEqual(version);
+        expect(responseEvent.id).toEqual(id);
+        expect(responseEvent.flowId).toEqual(flowId);
+        expect(responseEvent.payload).toEqual(message);
+        expect(responseEvent.auth).toEqual({});
+        expect(responseEvent.metadata).toEqual({});
+        expect(responseEvent.identity).toEqual({});
+    });
+
+    test("Should build response event error for badProtocol", () => {
+        
+        const name = "event:test";
+        const version = 1;
+        const id = "event-id";
+        const flowId = "event-flowId";
+        const identity = JSON.parse("{\"userId\": 1}");
+
+        const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{"test":"test"}}
+
+        const message: EventMessage =  {
+            "code": "INVALID_COMMUNICATION_PROTOCOL",
+            "parameters": {
+                "missingProperty": "some-missing"
+            } 
+          }
+
+        const responseEvent = buildBadProtocolFor(rawEvent, "some-missing")
+        
+        console.log(JSON.stringify(responseEvent))
+
+        expect(responseEvent.name).toEqual("badProtocol");
+        expect(responseEvent.version).toEqual(version);
+        expect(responseEvent.id).toEqual(id);
+        expect(responseEvent.flowId).toEqual(flowId);
+        expect(responseEvent.payload).toEqual(message);
+        expect(responseEvent.auth).toEqual({});
+        expect(responseEvent.metadata).toEqual({});
+        expect(responseEvent.identity).toEqual({});
+    });
+});
