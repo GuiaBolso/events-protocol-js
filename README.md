0d45db69116f68aaa1f3509dd982bacd4ed0ef8e
Octavio Turra <octavio@guiabolso.com.br>
2020-03-25 22:15:08 -0300

    :rotating_light: tentando configurar lib

--- README.md
+++ README.md
@@ -1,61 +1,74 @@
-  
-
 # Events Protocol JS
-Esta é uma implementação em javascript que atende aos requisitos do 
+
+Esta é uma implementação em javascript que atende aos requisitos do
 [Protocolo de eventos](https://github.com/GuiaBolso/events-protocol)
 
 ## Como usar
 
 ### Client
-A forma mais simples é através do generator `generateFetchEventByName`
-```js
-import  client  from  "@guiabolsobr/events-protocol/client";
 
-const  generateEvent = client.generateFetchEventByName({ // configuraçÕes
-	hostname:  "https://minha-url.com/comparador",
+A forma mais simples é através do generator `generateFetchEventByName`
+
+```js
+import client from "@guiabolsobr/events-protocol/client";
+
+const generateEvent = client.generateFetchEventByName({
+  // configuraçÕes
+  hostname: "https://minha-url.com/comparador"
 });
 
-const  base = generateEvent("test:event", {}, { isAuthorized:  true, auth: { token:  "my-token" } });
+const base = generateEvent(
+  "test:event",
+  {},
+  { isAuthorized: true, auth: { token: "my-token" } }
+);
 ```
 
 As configurações mais úteis são
+
 ```js
-hostname = string  // no default
-noauthURL = string  // default /others
+hostname = string; // no default
+noauthURL = string; // default /others
 ```
 
 ### Servidor
+
 Para utilizar do lado do servidor é necessário dois passos:
 
 O primeiro é registrar a função que deve ser chamada quando receber determinado evento, toda função deve receber e retornar um evento de resposta:
 Exemplo:
+
 ```js
 EventProcessor.addHandler("teste:event", 1, async (event: Event) => {
-	return  Promise.resolve(buildResponseEventFor(event));
+  return Promise.resolve(buildResponseEventFor(event));
 });
 ```
+
 Para processar o evento, após adicionar o handler basta apenas chamar o processador de evento.
 Exemplo:
 
 ```js
-EventProcessor.processEvent(event)
+EventProcessor.processEvent(event);
 ```
 
 A seguir um exemplo completo de uma Aws Lambda Handler completo:
+
 ```js
 EventProcessor.addHandler("teste:event", 1, async (event: Event) => {
-	//seu codigo aqui
-	return  Promise.resolve(buildResponseEventFor(event));
+  //seu codigo aqui
+  return Promise.resolve(buildResponseEventFor(event));
 });
 
-exports.handler = (event: any) : Promise<Event> => {
-	return  EventProcessor.processEvent(event);
+exports.handler = (event: any): Promise<Event> => {
+  return EventProcessor.processEvent(event);
 };
 ```
 
 ### Monitoração
-A lib já está pronta para usar o *Aws Xray*, basta apenas adicionar uma layer com esta dependência.
+
+A lib já está pronta para usar o _Aws Xray_, basta apenas adicionar uma layer com esta dependência.
 Com isso a lib irá gerar esta entrada em cada execução:
+
 ```json
 "subsegments": [
 {
--- src/server/tracer/tests/awsXrayInstrument.spec.ts
+++ src/server/tracer/tests/awsXrayInstrument.spec.ts
@@ -3,55 +3,71 @@ import instrumentExecutionOnXray from "../awsXrayInstrument";
 import { intoEvent, Event } from "../../../client/events";
 import { buildResponseEventFor } from "../../responseEventBuilder";
 
-
-
 const name = "event:test";
 const version = 1;
 const id = "event-id";
 const flowId = "event-flowId";
-const identity = JSON.parse("{\"userId\": 1}");
+const identity = JSON.parse('{"userId": 1}');
 
-const rawEvent = {name, version, id, flowId, identity, auth: {}, metadata:{}, payload:{}}
+const rawEvent = {
+  name,
+  version,
+  id,
+  flowId,
+  identity,
+  auth: {},
+  metadata: {},
+  payload: {}
+};
 
-const mockHandlerFunction = (event: Event) => Promise.resolve(buildResponseEventFor(intoEvent(rawEvent)))
+const mockHandlerFunction = (event: Event) =>
+  Promise.resolve(buildResponseEventFor(intoEvent(rawEvent)));
 
-describe("Test xray instrumentation",() => {
+describe("Test xray instrumentation", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
 
-    beforeEach(() => {
-        jest.clearAllMocks()
-        jest.resetModules()
+  test("When dependency is found should call instrumentation", async () => {
+    //Setup
+    const XRAY = require("aws-xray-sdk");
+    jest.mock("aws-xray-sdk");
+    const mockedSubSegment: Subsegment = new Subsegment("SubSegmentMocked");
+    mockedSubSegment.addNewSubsegment = jest
+      .fn()
+      .mockReturnValue(new Subsegment("NewSubsegmentMocked"));
+    XRAY.getSegment = jest.fn().mockReturnValue(mockedSubSegment);
+    //Execute
+    const responseEvent = await instrumentExecutionOnXray(
+      intoEvent(rawEvent),
+      mockHandlerFunction
+    );
+
+    //Assert
+    expect(mockedSubSegment.addNewSubsegment).toBeCalled();
+    expect(responseEvent.name).toBe(`${name}:response`);
+  });
+
+  test("When dependency is not found should not call instrumentation", async () => {
+    //Setup
+    //const XRAY = require("aws-xray-sdk")
+    jest.mock("aws-xray-sdk", () => {
+      throw new Error();
     });
+    const mockedSubSegment = new Subsegment("SubSegmentMocked");
+    mockedSubSegment.addNewSubsegment = jest
+      .fn()
+      .mockReturnValue(new Subsegment("NewSubsegmentMocked"));
 
-    test("When dependency is found should call instrumentation", async () => {
-        //Setup
-        const XRAY = require("aws-xray-sdk")
-        jest.mock("aws-xray-sdk")
-        const mockedSubSegment: Subsegment = new Subsegment("SubSegmentMocked")
-        mockedSubSegment.addNewSubsegment = jest.fn().mockReturnValue(new Subsegment("NewSubsegmentMocked"))
-        XRAY.getSegment = jest.fn().mockReturnValue(mockedSubSegment)
-        //Execute
-        const responseEvent = await instrumentExecutionOnXray(intoEvent(rawEvent), mockHandlerFunction)
-        
-        //Assert
-        expect(mockedSubSegment.addNewSubsegment).toBeCalled()
-        expect(responseEvent.name).toBe(`${name}:response`)
-    })
-    
-    test("When dependency is not found should not call instrumentation", async () => {
-        //Setup
-        //const XRAY = require("aws-xray-sdk")
-        jest.mock("aws-xray-sdk",() => {throw new Error()})
-        const mockedSubSegment = new Subsegment("SubSegmentMocked")
-        mockedSubSegment.addNewSubsegment = jest.fn().mockReturnValue(new Subsegment("NewSubsegmentMocked"))
-        
-        //Execute
-        const responseEvent = await instrumentExecutionOnXray(intoEvent(rawEvent), mockHandlerFunction)
-        
-        //Assert
-        expect(mockedSubSegment.addNewSubsegment).not.toBeCalled()
-        expect(responseEvent.name).toBe(`${name}:response`)
-        
-    })
-    
-})
+    //Execute
+    const responseEvent = await instrumentExecutionOnXray(
+      intoEvent(rawEvent),
+      mockHandlerFunction
+    );
 
+    //Assert
+    expect(mockedSubSegment.addNewSubsegment).not.toBeCalled();
+    expect(responseEvent.name).toBe(`${name}:response`);
+  });
+});
