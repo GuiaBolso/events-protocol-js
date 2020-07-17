67b87ad0ee8866f1c1e2928208f05354781819f9
Bruno Ortiz <brunortiz11@gmail.com>
2020-07-17 19:33:07 -0300

    more client tests

--- src/client/tests/client.spec.ts
+++ src/client/tests/client.spec.ts
@@ -1,7 +1,8 @@
 import EventsClient from "client/client";
 import { getUUID } from "utils";
 import { Event } from "core/events";
-import { EventError, isEventError, isSuccess } from "client/response";
+import { isEventError, isFailedDependency, isSuccess } from "client/response";
+import { BadRequest, GenericError } from "core/errors";
 
 describe("Test EventClient", () => {
     const event = {
@@ -52,6 +53,58 @@ describe("Test EventClient", () => {
 
         expect(isEventError(response)).toBeTruthy();
 
+        if (isEventError(response)) {
+            expect(response.errorType).toBe(GenericError);
+        }
+        done();
+    });
+
+    test("event error is badRequest", async done => {
+        const mockJsonPromise: Event = {
+            ...event,
+            name: `${event.name}:badRequest`
+        };
+        const mockFetchPromise = Promise.resolve({
+            status: 200,
+            json: () => mockJsonPromise
+        });
+
+        const fetchMock = jest.fn().mockReturnValue(mockFetchPromise);
+
+        const client = new EventsClient("https://some.url", {
+            fetchHandler: fetchMock
+        });
+        const response = await client.sendEvent(event);
+
+        expect(isEventError(response)).toBeTruthy();
+
+        if (isEventError(response)) {
+            expect(response.errorType).toBe(BadRequest);
+        }
+        done();
+    });
+
+    test("event timeout error", async done => {
+        async function delay(ms: number): Promise<any> {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        const defaultTimeout = 100;
+        const fetchMock = jest
+            .fn()
+            .mockImplementation(async () => await delay(300));
+
+        const client = new EventsClient("https://some.url", {
+            fetchHandler: fetchMock,
+            defaultTimeout: defaultTimeout
+        });
+        const response = await client.sendEvent(event);
+
+        expect(isFailedDependency(response)).toBeTruthy();
+
+        if (isFailedDependency(response)) {
+            expect(response.error.includes("Timeout")).toBeTruthy();
+        }
         done();
     });
 });
